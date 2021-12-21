2452a92f9ae13f17fa7a9a272d42cbd8d985b7b8
Tom McDingus <mcdowelltomasz@gmail.com>
2021-12-21 17:34:29 +1300

    add about me section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <main class="max-h-screen overflow-y-scroll snap snap-y snap-mandatory">
   <section class="flex items-center justify-center w-full h-screen bg-indigo-200 snap-start">
-  <strong className="bg-white-400 px-1 text-7xl">
+  <strong className="bg-white-400 px-1 text-7xl font-sans">
         <Typical
              steps={[
                   'tomasz',
@@ -26,11 +26,26 @@ function App() {
   </strong>
   </section>
   <section class="flex items-center justify-center w-full h-screen snap-start grid grid-cols-5 gap-3 text-center">
-    <div class="bg-blue-100">
+    <div class="px-2">
       <img src={portrait} alt="Me" class="rounded-full self-center"/>
-      <a href="https://github.com/tamosz" class="no-underline hover:underline">github</a>
+      <a href="https://github.com/tamosz" class="no-underline hover:underline font-mono text-xs">visit my github</a>
     </div>
-    <div class="bg-red-100 col-span-4">2nd col</div>
+    <div class="col-span-4 font-sans text-justify px-10">
+      <p>Hi there! I’m Tom. I’m a passionate introvert who loves building things with code. 
+        I've been attracted to programming since I was 12, due to my love of the internet, 
+        computers, and gaming. I just wanted to know more about how everything worked, how 
+        computers could do all the amazing things they can do. So, I downloaded a PDF of C++ 
+        programming fundamentals, recommended to me in an IRC channel. It was several 
+        hundred pages, and totally inscrutable to me. </p>
+                <br></br>
+      <p>After several years, and a few false starts at University later I decided to try again.
+         It was a radically different experience. Online resources have come such a long way. Since then, I’ve been honing my skills 
+         and learning JavaScript. I recently graduated from Open Polytechnic's NZ Web Development 
+         and Design Diploma.</p> 
+                <br></br>
+      <p>I’m seeking a full-time role where I can help a company achieve their goals, and learn 
+        from real developers to grow my skills and further my understanding of the digital world.</p>
+</div>
   </section>
   <section class="w-full h-screen bg-green-200 snap-start">
     Section 3
